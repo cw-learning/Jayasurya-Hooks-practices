180fec4baa7350e49e40ff2ab7aa2bc8ab1600a9
jayasuryavelusamy04 <jayasuryavelusamy@gmail.com>
2026-01-27 15:08:13 +0530

    fix(Prettier): Run prettier and fix the issues

--- eslint.config.js
+++ eslint.config.js
@@ -17,19 +17,16 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      'prettier': prettier,
+      prettier: prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'prettier/prettier': 'error',
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
     },
-  },
+  }
 );
